5084f5085222f86e602ef9909ca53dc612311e35
xxjcaxx <jocasal@gmail.com>
2025-11-24 12:00:44 +0100

    sdf

--- src/router.js
+++ src/router.js
@@ -4,7 +4,7 @@ import { renderProfile } from "./components/profile";
 
 export {router}
 
-const routes = new Map([
+/*const routes = new Map([
     ['',renderContent],
     ['#game',renderContent],
     ['#login',renderLogin('login')],
@@ -16,6 +16,23 @@ const routes = new Map([
 function router(route,container){
     if(routes.has(route)){
         container.replaceChildren(routes.get(route)());
+    }
+    else {
+        container.innerHTML = `<h2>404</h2>`
+    }
+}*/
+const routes = new Map([
+    ['','game-content'],
+    ['#game','game-content'],
+    ['#login',renderLogin('login')],
+    ['#register',renderLogin('register')],
+    ['#profile',renderProfile]
+])
+
+
+function router(route,container){
+    if(routes.has(route)){
+        container.replaceChildren(document.createElement(routes.get(route)));
     }
     else {
         container.innerHTML = `<h2>404</h2>`
